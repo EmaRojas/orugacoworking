61645c4adb4a4c2821626f7664512a916fe85070
EmaRojas <emarojas710@gmail.com>
2023-04-28 16:09:14 -0300

    add class client

--- models/client.js
+++ models/client.js
@@ -1,10 +1,35 @@
 const mongoose = require("mongoose")
 
 const ClientSchema = new mongoose.Schema({
-    name:{
+    full_name:{
         type:String,
         require: true
+    },
+    phone:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    company_name:{
+        type:String,
+        require:true
+    },
+    cuit:{
+        type:String,
+        require:true
+    },    
+    description:{
+        type:String,
+        require:true
+    },
+    assistance:{
+        type:String,
+        require:true
     }
+
 });
 
 module.exports = mongoose.model("Client", ClientSchema);
--- routes/ClientRouter.js
+++ routes/ClientRouter.js
@@ -3,54 +3,91 @@ const Client = require("../models/client")
 const ClientRouter = express.Router();
 
 
-ClientRouter.get("/", async(req,res) => {
+ClientRouter.get("/", async (req, res) => {
   let clients = await Client.find({})
   return res.status(200).send({
-    success:true,
+    success: true,
     clients,
   });
 });
 
-ClientRouter.post("/client", async (req,res)=>{
-  try{
-      const {name}= req.body;
-      console.log(req.body);
-      console.log(name);
-      if(!name)
-      {
-        return res.status(400).send({
-          success:false,
-          message:"Faltan datos"
-        });
-      }
-
-      let client = new Client({
-        name
+ClientRouter.post("/client", async (req, res) => {
+  try {
+    const { full_name, phone, email, company_name, cuit, description, assistance } = req.body;
+    console.log(req.body);
+    if (!full_name || !phone || !email || !company_name || !cuit || !description || !assistance) {
+      return res.status(400).send({
+        success: false,
+        message: "Faltan datos de completar"
       });
+    }
 
-      await client.save()
+    let client = new Client({
+      full_name,
+      phone,
+      email,
+      company_name,
+      cuit,
+      description,
+      assistance
+    });
 
-      return res.status(200).send({
-        success:true,
-        message: "Cliente Creado",
-        client
-      });
+    await client.save()
 
-  }catch(err){
+    return res.status(200).send({
+      success: true,
+      message: "Cliente Creado",
+      client
+    });
+
+  } catch (err) {
     return res.status(500).send({
-      success:false,
+      success: false,
       message: err.message
+    });
+  }
+});
+
+//get all
+ClientRouter.get("/", async (req, res) => {
+  let clients = await Client.find({});
+  return res.status(200).send({
+    success: true,
+    clients
+  });
+});
+
+//update
+ClientRouter.put("/update/:id", async (req, res) => {
+  console.log(req.body);
+  const { id } = req.params;
+  const { ...data} = req.body;
+
+  let client = await Client.findByIdAndUpdate(id, data, {new:true});
+
+  res.status(200).send({
+    success:true,
+    message: "Cliente modificado!",
+    client
+  });
+});
+
+ClientRouter.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Client.findByIdAndDelete(id);
+    res.status(200).send({
+      success: true,
+      message: "Cliente eliminado!",
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: error.message,
     });
   }
 });
 
 module.exports = ClientRouter
 
-// router.get("/", async (req, res, next) => {
-//   return res.status(200).json({
-//     title: "Express Testing",
-//     message: "The app is working properly!",
-//   });
-// });
 
-// module.exports = router;
